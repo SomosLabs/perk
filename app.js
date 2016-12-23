3f5cd52d17de6ec75e7a122194f2926e45300c29
josebet1 <jose@joseb.me>
2016-12-23 03:33:40 -0600

    initial work with pug

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+
+const authSecret = process.env.AUTH_SECRET;
+
+app.set('view engine', 'pug');
+// mongoose.connect('mongodb://localhost/perk');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(require('./routes/index.js'));
+// app.use(require('./routes/user.js'));
+
+module.exports = app;
+console.log('Perk is perking.');
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // =============================================================================
 const app = require('./app.js');
 
-app.set('port', process.env.PORT || 61337);
+app.set('port', process.env.PORT || 61338);
 
 const server = app.listen(app.get('port'), () => {
   console.log(`server listening on port ${server.address().port} `);
