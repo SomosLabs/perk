ca2b8a097e0f485ede30cc37fc47ab3e64eacc43
josebet1 <josebet1@apollo.local>
2016-12-22 23:11:56 -0600

    not your average init commit

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  first: String,
+  last: String,
+  email: String,
+  phone: String,
+  hasJob: Boolean,
+  resume: { type: Schema.ObjectId, ref: 'Resume' },
+  auth: [{
+    provider: String,
+    id: String,
+    oauth_token: String,
+    oauth_expiration: Date,
+    last_login: Date,
+  }],
+  perks: [{
+    id: { type: Schema.ObjectId, ref: 'Perk' },
+    used: Boolean,
+    used_on: Date,
+  }],
+  companies: [{
+    id: { type: Schema.ObjectId, ref: 'Company' },
+    applied: Boolean,
+    applied_on: Date,
+  }],
+
+}, { timestamps: true }, { collection: 'users' });
+
+const UserModel = mongoose.model('User', UserSchema);
+module.exports.User = UserModel;
+module.exports.Schema = UserSchema;
