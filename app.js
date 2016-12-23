6a4b5f6c0483fbf41cfe04b4659346e460b028d0
josebet1 <jose@joseb.me>
2016-12-23 13:06:28 -0600

    passport is not that cool

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
-'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
 const app = express();
 
 const authSecret = process.env.AUTH_SECRET;
+const googleClientID = process.env.GOOGLE_CLIENT_ID;
+const googleCientSecret = process.env.GOOGLE_CLIENT_SECRET;
+const googleCBURL = process.env.GOOGLE_CB_URL;
 
 app.set('view engine', 'pug');
 // mongoose.connect('mongodb://localhost/perk');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+passport.use(new GoogleStrategy({
+  clientID: googleClientID,
+  clientSecret: googleCientSecret,
+  callbackURL: googleCBURL,
+}, (accessToken, refreshToken, profile, cb) => {
+	return cb(null, profile);
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+
+passport.deserializeUser((obj, cb) => {
+  cb(null, obj);
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(require('./routes/index.js'));
 // app.use(require('./routes/user.js'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
+const passport = require('passport');
 const express = require('express');
+const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
+
 const router = express.Router();
 
 router.route('/')
   .get((req, res) => {
-    res.render('index', { title: 'Hey', message: 'Hello there!' })
+    res.render('index', { title: 'Hey', message: 'Hello there!' });
+  });
+
+router.route('/login')
+  .get((req, res) => {
+    res.render('login');
+  });
+
+router.route('/auth/google').get(passport.authenticate('google', { scope: ['profile'] }));
+
+router.route('/auth/google/callback')
+  .get(passport.authenticate('google', { failureRedirect: '/' }),
+    (req, res) => {
+      res.redirect('/testing');
+    });
+
+router.route('/logout')
+  .get((req, res) => {
+    req.logout();
+    res.redirect('/');
   });
 
 module.exports = router;
